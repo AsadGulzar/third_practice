39b9c81fd76f5ce44aca07000ad08cdb1d9489ea
AsadGulzar <Asadgujjar255@gmail.com>
2026-05-01 21:59:27 +0500

    all file data insert

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        task = request.form.get('task')
+        if task:
+            tasks.append(task)
+        return redirect(url_for('view_tasks'))
+    return render_template('add_task.html')
+
+
+@app.route('/tasks')
+def view_tasks():
+    return render_template('tasks.html', tasks=tasks)
+
+
+@app.route('/delete/<int:index>')
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+    return redirect(url_for('view_tasks'))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
